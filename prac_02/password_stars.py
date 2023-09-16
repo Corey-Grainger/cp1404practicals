7e62dde05a0e999c735301526fc91462d5c8e74b
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-09-16 20:41:49 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,22 @@
 """Password Stars"""
 
-minimum_password_length = 8
-password_entered = input("Enter password: ")
-while len(password_entered) < 8:
-    print("Password must be 8 characters or more")
+MINIMUM_PASSWORD_LENGTH = 8
+
+def main():
+    password_entered = get_password()
+    print_line_of_characters(len(password_entered))
+
+
+def print_line_of_characters(length):
+    print("*" * length)
+
+
+def get_password():
     password_entered = input("Enter password: ")
-print("*" * len(password_entered))
+    while len(password_entered) < 8:
+        print("Password must be 8 characters or more")
+        password_entered = input("Enter password: ")
+    return password_entered
+
+
+
