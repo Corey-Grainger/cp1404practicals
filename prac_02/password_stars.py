a5e1d7c4f6d0f1f9cbe16d7597d0902b710ca3bf
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-09-16 20:52:05 +1000

    Updated minimum password length to a constant passed into the get password function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,7 +4,7 @@ MINIMUM_PASSWORD_LENGTH = 8
 
 
 def main():
-    password_entered = get_password()
+    password_entered = get_password(MINIMUM_PASSWORD_LENGTH)
     print_line_of_characters(len(password_entered))
 
 
@@ -12,9 +12,10 @@ def print_line_of_characters(length):
     print("*" * length)
 
 
-def get_password():
+def get_password(minimum_password_length):
+    """Get a password with a minimum length"""
     password_entered = input("Enter password: ")
-    while len(password_entered) < 8:
+    while len(password_entered) < minimum_password_length:
         print("Password must be 8 characters or more")
         password_entered = input("Enter password: ")
     return password_entered
