9e9de19f724a3e002fb59f00b207df3e9be6e289
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-09-19 12:39:36 +1000

    add random number of smiley faces program to do this now program

--- prac_02/do_this_now.py
+++ prac_02/do_this_now.py
@@ -1,2 +1,14 @@
 
-"""Do This Now area for working on miscellaneous seminar and prac activities"""
+"""Do This Now area for working on seminar and prac activities"""
+
+import random
+
+low_number = int(input("Enter low number: "))
+high_number = int(input("Enter high number: "))
+while high_number <= low_number:
+    print("High number must be higher than low number")
+    high_number = int(input("Enter high number: "))
+n = random.randint(low_number,high_number)
+print("=) " * n)
+
+
