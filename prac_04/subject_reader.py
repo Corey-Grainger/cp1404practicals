1e5fc4775f575b70fe602a7c4ca8be8ca5a93255
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-09-30 16:16:41 +1000

    Refactor subject reader program to return subject data and format printing of subject data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,21 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    for datum in data:
+        print(f"{datum[0]} is taught by {datum[1]:12} and has {datum[2]:3} students")
 
 
 def get_data():
-    """Read data from file formatted like: subject,lecturer,number of students."""
+    """Return subject data as a list from a file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])  # Will be valid int in correctly formatted file
+        data.append(parts)
     input_file.close()
+    return data
 
 
 main()
