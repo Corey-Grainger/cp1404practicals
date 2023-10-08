fe3ac42bc183d69a230025b2ffef6a92990ad0cc
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-10-08 18:17:57 +1000

    Reformat code with a function for inferring name from email and add missing docstring

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,11 +3,11 @@
 
 
 def main():
+    """Create a dictionary of email addresses to names."""
     email_to_name = {}
     email = get_valid_email()
     while email != "":
-        email_name_components = email[0:email.find("@")].split(".")
-        name = ' '.join(email_name_components)
+        name = get_name_from_email(email)
         name_confirmation = input(f"Is your name {name}? (Y/n)").lower()
         if name_confirmation != "y" and name_confirmation != "":
             name = input("Name: ")
@@ -17,11 +17,19 @@ def main():
 
 
 def get_valid_email():
+    """Get a valid email address."""
     email = input("Email: ")
     while "@" not in email:
-        print("Invalid email, email must contain @ character")
+        print("Invalid email, email must contain @")
         email = input("Email: ")
     return email
+
+
+def get_name_from_email(email):
+    """Get an inferred name from an email address"""
+    email_name_components = email[0:email.find("@")].split(".")
+    name = ' '.join(email_name_components)
+    return name
 
 
 main()
