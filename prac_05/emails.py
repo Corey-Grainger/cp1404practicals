aee58ada8b5fc16a291914de22de9297a666b87e
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-10-08 17:22:59 +1000

    Update emails program to add name email pairs to dictionary

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,11 +1,27 @@
 """CP1404 Week 05 Practicals
  Emails practice program"""
 
-email_to_name = {}
-email = input("Email: ")
-while email != "":
+
+def main():
+    email_to_name = {}
+    email = get_valid_email()
+    while email != "":
+        email_name_components = email[0:email.find("@")].split(".")
+        name = ' '.join(email_name_components)
+        name_confirmation = input(f"Is your name {name}? (Y/n)").lower()
+        if name_confirmation != "y" and name_confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        print(email_to_name)
+        email = get_valid_email()
+
+
+def get_valid_email():
+    email = input("Email: ")
     while "@" not in email:
         print("Invalid email, email must contain @ character")
         email = input("Email: ")
-    email_name_components = email[0:email.find("@")].split(".")
-    print(email_name_components)
+    return email
+
+
+main()
