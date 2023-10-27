c2bbac7be47f3bcf3bbea1972dafb1fa7982f299
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-10-27 10:07:31 +1000

    Add docstrings to the guitar class

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -6,19 +6,27 @@ Completed: 3:06pm"""
 
 
 class Guitar:
+    """Guitar class"""
+    CURRENT_YEAR = 2023
+    VINTAGE_THRESHOLD = 50
+
     def __init__(self, name="", year=1590, cost=0.0):
+        """Construct the guitar class with name, year and cost attributes."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """String version of guitar class."""
         return f"{self.name} ({self.year}) : ${self.cost:.2f})"
 
-    def get_age(self):
-        return 2023 - self.year
+    def get_age(self, current_year):
+        """Gets the age of guitar by subtracting its year of manufacture from current year."""
+        return current_year - self.year
 
-    def is_vintage(self):
-        return (self.get_age()) >= 50
+    def is_vintage(self, vintage_threshold):
+        """Determine if guitar is vintage"""
+        return (self.get_age()) >= vintage_threshold
 
 
 if __name__ == '__main__':
