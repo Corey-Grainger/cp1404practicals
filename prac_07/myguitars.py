6bbdc4121393c4c61e7da2dd33b273b6301da7bf
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-10-31 16:44:13 +1000

    Add menu loop to main, functions to add and save guitars, and extract displaying of guitars into a function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -8,6 +8,7 @@ from guitar import Guitar
 import csv
 
 FILENAME = "guitars.csv"
+MENU = "(A)dd guitars\n(D)isplay Guitars\nSave an (Q)uit"
 
 
 def main():
@@ -16,9 +17,37 @@ def main():
     guitars = load_guitars(FILENAME)
     guitars.sort()
     print(f"{len(guitars)} guitars loaded.")
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "D":
+            display_guitars(guitars)
+        elif choice == "A":
+            add_new_guitars(guitars)
+        else:
+            print("Invalid selection.")
+        print(MENU)
+        choice = input(">>> ").upper()
+    save_guitars(FILENAME, guitars)
+
+
+def display_guitars(guitars):
+    """Display each guitar in guitars"""
     print(f"You have the following guitars:")
     for guitar in guitars:
-        print(f"{guitar.name} is from {guitar.year} and costs {guitar.cost}")
+        print(f"{guitar.name:30} is from {guitar.year:4} and costs {guitar.cost:.2f}")
+
+
+def add_new_guitars(guitars):
+    """Get new guitars until the user enters an empty string."""
+    print("Enter details of new guitars.  Enter a blank name to stop.")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        guitars.append(Guitar(name, year, cost))
+        print(f"{name} ({year}) : ${cost:.2f} added.")
+        name = input("Name: ")
 
 
 def load_guitars(filename):
@@ -29,6 +58,14 @@ def load_guitars(filename):
         for row in reader:
             guitars.append(Guitar(row[0], int(row[1]), float(row[2])))
     return guitars
+
+
+def save_guitars(filename, guitars):
+    """Save guitars to filename as a CSV file with each line formatted like: Name,Year,Cost."""
+    with open(filename, "w", newline='') as out_file:  # use default dialect
+        writer = csv.writer(out_file)
+        for guitar in guitars:
+            writer.writerow([guitar.name, guitar.year, guitar.cost])
 
 
 main()
