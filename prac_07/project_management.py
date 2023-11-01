bfa87708eddef5f28e692dcf32afa30128b219a0
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-11-01 11:07:19 +1000

    Reformat error checking to exception handling method

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,15 +2,20 @@
 Project Management Program
 Estimate: 3 hours
 Commenced: 6:55pm
-Completed: """
+Break: 9:30pm
+Recommenced 10:00am
+Completed: 11:00am next day"""
 
 import datetime
 from project import Project
 from operator import attrgetter
+from fnmatch import fnmatch
 
+LOWEST_PRIORITY = 10
+HIGHEST_PRIORITY = 1
 MENU = ("(L)oad projects\n(S)ave projects\n(D)isplay projects\n"
         "(F)ilter projects by date\n(A)dd new project\n(U)pdate project\n(Q)uit")
-DEFAULT_FILENAME = "projects.txt"
+DEFAULT_FILENAME = "project.txt"
 
 
 def main():
@@ -23,21 +28,24 @@ def main():
             if projects:
                 continue_choice = input("Unsaved projects will be lost, continue? (y/N): ").upper()
                 if continue_choice == "Y":
-                    filename = get_valid_filename()
-                    load_project_file(filename)
+                    filename = get_valid_filename("Enter filename to load (e.g. filename.txt): ")
+                    projects = load_project_file(filename)
                 else:
                     print("File loading aborted.")
+            else:
+                filename = get_valid_filename("Enter filename to load (e.g. filename.txt): ")
+                projects = load_project_file(filename)
         elif menu_choice == "S":
-            filename = get_valid_filename()
+            filename = get_valid_filename("Enter name for save file (e.g. filename.txt): ")
             save_projects(filename, projects)
         elif menu_choice == "D":
-            display_projects(projects)
+                display_projects(projects)
         elif menu_choice == "F":
             selected_filter_date = get_valid_date("Show projects that start after date (dd/mm/yyyy): ")
             filter_projects_by_date(projects, selected_filter_date)
         elif menu_choice == "A":
             print("Let's add a new project")
-            add_a_project(projects)
+            add_new_project(projects)
         elif menu_choice == "U":
             update_project(projects)
         else:
@@ -50,15 +58,19 @@ def main():
 
 def load_project_file(filename):
     """Load projects from filename."""
-    with open(filename) as in_file:
-        # Ignores first line of row headings
-        projects = []
-        in_file.readline()
-        for line in in_file:
-            parts = line.split("\t")
-            parts[1] = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
-            projects.append(Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4])))
-    return projects
+    try:
+        with open(filename) as in_file:
+            # Ignores first line of row headings
+            projects = []
+            in_file.readline()
+            for line in in_file:
+                parts = line.split("\t")
+                parts[1] = datetime.datetime.strptime(parts[1], "%d/%m/%Y").date()
+                projects.append(Project(parts[0], parts[1], int(parts[2]), float(parts[3]), int(parts[4])))
+            return projects
+    except FileNotFoundError:
+        print("Filename selected for loading was not found.")
+        pass
 
 
 def save_projects(filename, projects):
@@ -71,41 +83,58 @@ def save_projects(filename, projects):
     print(f"{len(projects)} projects saves to {filename}")
 
 
-def get_valid_filename():
+def get_valid_filename(prompt):
     """Get a valid filename like filename.txt"""
-    filename = input("Enter filename to load (e.g. filename.txt): ")
+    filename = input(prompt)
+    while not fnmatch(filename, "*.txt"):
+        print("Invalid filename.")
+        filename = input(prompt)
     return filename
 
 
 def display_projects(projects):
-    """Displays projects """
-    print("Incomplete projects: ")
-    for project in sorted([project for project in projects if not project.is_complete()]):
-        print(f"\t{project}")
-    print("Completed projects: ")
-    for project in sorted([project for project in projects if project.is_complete()]):
-        print(f"\t{project}")
+    """Displays incomplete projects and then complete projects sorted into """
+    try:
+        print("Incomplete projects: ")
+        for project in sorted([project for project in projects if not project.is_complete()]):
+            print(f"\t{project}")
+        print("Completed projects: ")
+        for project in sorted([project for project in projects if project.is_complete()]):
+            print(f"\t{project}")
+    except TypeError:
+        print("No projects to display.")
 
 
 def filter_projects_by_date(projects, date):
     """Display projects started after date"""
-    for project in sorted([project for project in projects if project.start_date > date], key=attrgetter("start_date")):
-        print(f"\t{project}")
-
+    try:
+        for project in sorted([project for project in projects if project.start_date > date], key=attrgetter("start_date")):
+            print(f"\t{project}")
+    except TypeError:
+        print("No projects to display.")
 
 def get_valid_date(prompt):
     """Get a valid date."""
     # Extract the date from a user input like 11/12/13
-    date = datetime.datetime.strptime(input(prompt), "%d/%m/%Y").date()
+    is_valid_date = False
+    while not is_valid_date:
+        try:
+            date = datetime.datetime.strptime(input(prompt), "%d/%m/%Y").date()
+            if date > datetime.date.today():
+                print("Start date cannot be after today")
+            else:
+                is_valid_date = True
+        except ValueError:
+            print("Invalid date.  Date must be in format dd/mm/yyyy")
     return date
 
 
-def add_a_project(projects):
+def add_new_project(projects):
     """Add a new project to projects."""
     name = get_valid_string()
     start_date = get_valid_date("Start date (dd/mm/yyyy): ")
-    priority = get_valid_number(1, 10, "Priority: ")
-    cost_estimate = get_valid_cost_estimate()
+    priority = get_valid_number(HIGHEST_PRIORITY, LOWEST_PRIORITY, "Priority: ")
+    cost_estimate = get_valid_cost_estimate(0)
     completion_percentage = get_valid_number(0, 100, "Percent complete: ")
     projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
 
@@ -121,25 +150,46 @@ def get_valid_string():
 
 def get_valid_number(minimum, maximum, prompt):
     """Get a valid number between minimum and maximum inclusive."""
-    number = int(input(prompt))
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            number = int(input(prompt))
+            if minimum <= number <= maximum:
+                is_valid_input = True
+            else:
+                print(f"Input must > {minimum} and < {maximum}")
+        except ValueError:
+            print("Invalid input")
     return number
 
 
-def get_valid_cost_estimate():
+def get_valid_cost_estimate(minimum):
     """Get a cost estimate that isn't negative."""
-    cost_estimate = float(input("Cost estimate: $"))
-    return cost_estimate
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            cost_estimate = float(input("Cost estimate: $"))
+            if cost_estimate > 0:
+                is_valid_input = True
+            else:
+                print("Cost estimate must be > 0")
+        except ValueError:
+            print("Invalid input")
+    return cost_estimate  # Error checking prevents variable being unassigned
 
 
 def update_project(projects):
     """Update the priority and/or completion percentage of a project."""
-    for i, project in enumerate(projects):
-        print(f"{i} {project}")
-    project_choice = get_valid_number(0, len(projects) - 1, "Project choice: ")
-    new_percentage = get_valid_new_value(0, 100, "New Percentage: ", projects[project_choice].completion_percentage)
-    projects[project_choice].completion_percentage = new_percentage
-    new_priority = get_valid_new_value(0, 100, "New Priority: ", projects[project_choice].priority)
-    projects[project_choice].priority = new_priority
+    try:
+        for i, project in enumerate(projects):
+            print(f"{i} {project}")
+        project_choice = get_valid_number(0, len(projects) - 1, "Project choice: ")
+        new_percentage = get_valid_new_value(0, 100, "New Percentage: ", projects[project_choice].completion_percentage)
+        projects[project_choice].completion_percentage = new_percentage
+        new_priority = get_valid_new_value(0, 100, "New Priority: ", projects[project_choice].priority)
+        projects[project_choice].priority = new_priority
+    except TypeError:
+        print("No projects to display.")
 
 
 def get_valid_new_value(minimum, maximum, prompt, project_value):
@@ -156,7 +206,7 @@ def get_valid_new_value(minimum, maximum, prompt, project_value):
         else:
             number = project_value
             is_valid_value = True
-    return number
+    return number  # Error checking prevents variable being unassigned
 
 
 main()
