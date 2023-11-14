d8ab7fda608f91bc7252d0184be77ac80cf96311
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-11-14 15:44:21 +1000

    Add Band class for my_band program

---
+++ prac_09/band.py
@@ -0,0 +1,26 @@
+"""CP1404 Week 09 Prac
+Band Class
+Estimated: 20 minutes
+Commenced 1:34pm
+Completed """
+
+class Band():
+
+    def __init__(self, name):
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        musician_strings = []
+        for musician in self.musicians:
+            musician_strings.append(str(musician))
+        return f"{self.name} ({','.join(musician_strings)})"
+
+    def add(self, musician):
+        self.musicians.append(musician)
+
+    def play(self):
+        play_strings = []
+        for musician in self.musicians:
+            play_strings.append(musician.play())
+        return ",".join(play_strings)
