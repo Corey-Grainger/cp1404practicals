b71c93f0abce69d10f1d15346236ccb97b8b7c8a
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-11-14 10:20:26 +1000

    Add Car and Taxi classes and taxi_test program to test Taxi class

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,15 @@
+"""CP1404 Week 09 Pracs
+Taxi Class Test Program
+Commenced: 10:10am
+Completed: 10:19am"""
+
+from taxi import Taxi
+
+my_taxi = Taxi("Prius", 100, 1.23)
+my_taxi.drive(40)
+print(my_taxi)
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(my_taxi)
+current_fare = my_taxi.current_fare_distance * my_taxi.price_per_km
+print(f"Current fare for {my_taxi.name} is ${current_fare:.2f}")
