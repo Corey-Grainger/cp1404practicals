1eb48e20635c80819cd83934bf232019d4a14734
Corey Grainger <corey.grainger@my.jcu.edu.au>
2023-11-16 14:29:37 +1000

    Add missing docstrings to Band class

--- prac_09/band.py
+++ prac_09/band.py
@@ -7,19 +7,23 @@ Completed 2:03pm"""
 class Band():
 
     def __init__(self, name):
+        """Initialise a band."""
         self.name = name
         self.musicians = []
 
     def __str__(self):
+        """Return the string version of a band."""
         musician_strings = []
         for musician in self.musicians:
             musician_strings.append(str(musician))
         return f"{self.name} ({','.join(musician_strings)})"
 
     def add(self, musician):
+        """Add a Musician to a bands list of musicians."""
         self.musicians.append(musician)
 
     def play(self):
+        """Return each bands members play() contribution."""
         play_strings = []
         for musician in self.musicians:
             play_strings.append(musician.play())
